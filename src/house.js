09adc34a008b679007c48eb3230fd33169f72e7f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-06-08 11:53:53 +0200

    Print the first two lines

--- src/house.js
+++ src/house.js
@@ -1,6 +1,13 @@
 class House {
-  recite() {
-    return "This is the house that Jack built.\n"
+  line(number) {
+    return `This is ${this.data().slice(-number).join(' ')}.\n`;
+  }
+
+  data() {
+    return [
+      "the malt that lay in",
+      "the house that Jack built",
+    ];
   }
 }
 
--- test/house_test.js
+++ test/house_test.js
@@ -5,7 +5,13 @@ const {House} = require('../src/house');
 describe('The house that Jack built', function() {
   it('prints the first line', function() {
     const house = new House();
-    const result = house.recite();
+    const result = house.line(1);
     expect(result).to.equal('This is the house that Jack built.\n')
+  });
+
+  it('prints the second line', function() {
+    const house = new House();
+    const result = house.line();
+    expect(result).to.equal('This is the malt that lay in the house that Jack built.\n')
   });
 });
