61a23d18c8ad47226198c9a91898a16d5f88711b
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-06-15 12:18:40 +0200

    Add docs and decompose the base behavior

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Starting point of a kata to explore inheritance vs composition, following this talk:
 
 - https://www.youtube.com/watch?v=OMPfEXIlTVE
+- https://speakerdeck.com/skmetz/nothing-is-something-railsconf
--- src/house.js
+++ src/house.js
@@ -1,18 +1,6 @@
-class House {
-  recite() {
-    const lines = [];
-    for (let i = 1; i <= this.data().length; ++i) {
-      lines.push(this.line(i));
-    }
-    return lines.join('\n');
-  }
+const _ = require('lodash');
 
-  line(number) {
-    return `This is ${this.data().slice(-number).join(' ')}.\n`;
-  }
-
-  data() {
-    return [
+const DATA = [
       'the horse and the hound and the horn that belonged to',
       'the farmer sowing his corn that kept',
       'the rooster that crowed in the morn that woke',
@@ -26,9 +14,39 @@ class House {
       'the malt that lay in',
       'the house that Jack built',
     ];
+
+class House {
+  recite() {
+    const lines = [];
+    for (let i = 1; i <= this.data().length; ++i) {
+      lines.push(this.line(i));
+    }
+    return lines.join('\n');
+  }
+
+  line(number) {
+    return `This is ${this.phrase(number)}.\n`;
+  }
+
+  phrase(number) {
+    return this.parts(number).join(' ');
+  }
+
+  parts(number) {
+    return this.data().slice(-number);
+  }
+
+  data() {
+    this._data = DATA;
+    return this._data;
   }
 }
 
 module.exports = {
   House,
+}
+
+if (require.main === module) {
+  const house = new House();
+  console.log(house.recite());
 }
